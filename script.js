14d4ac1c5e9eb4f7c479ac13c8d1af686a47f28d
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-06-10 11:43:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-//your code here
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Rolling Stones', 'Queen'];
 
-// Define the array of band names
-let bandNames = [
-  'The Rolling Stones',
-  'AC/DC',
-  'Led Zeppelin',
-  'Pink Floyd',
-  'The Beatles',
-];
+function sortBandNames(bandNames) {
+  const articles = ['a', 'an', 'the'];
 
-// Function to remove articles from the band name
-function removeArticles(bandName) {
-  return bandName.replace(/^(a |an |the )/i, '').trim();
+  // Remove articles from band names
+  const cleanedNames = bandNames.map(name => {
+    // Split the name into individual words
+    const words = name.split(' ');
+
+    // Filter out the articles
+    const filteredWords = words.filter(word => !articles.includes(word.toLowerCase()));
+
+    // Join the remaining words back together
+    const cleanedName = filteredWords.join(' ');
+
+    return cleanedName;
+  });
+
+  // Sort the band names in lexicographic order
+  const sortedNames = cleanedNames.sort((a, b) => a.localeCompare(b));
+
+  // Display the sorted names in the HTML page
+  const ulElement = document.getElementById('band');
+  sortedNames.forEach(name => {
+    const liElement = document.createElement('li');
+    liElement.textContent = name;
+    ulElement.appendChild(liElement);
+  });
 }
 
-// Sort the band names in lexicographic order
-bandNames.sort(function (a, b) {
-  const bandA = removeArticles(a);
-  const bandB = removeArticles(b);
-  if (bandA < bandB) {
-    return -1;
-  }
-  if (bandA > bandB) {
-    return 1;
-  }
-  return 0;
-});
-
-// Get the <ul> element by its ID
-const ulElement = document.getElementById('bands');
-
-// Iterate over the sorted band names and create <li> elements
-bandNames.forEach(function (bandName) {
-  const liElement = document.createElement('li');
-  liElement.textContent = bandName;
-  ulElement.appendChild(liElement);
-});
+// Call the function with the band names array
+sortBandNames(bandNames);
